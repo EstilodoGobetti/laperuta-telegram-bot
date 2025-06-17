b291817f64381ec0feda914d07199b5987b99927
EstilodoGobetti <rodrigogobettimiranda@gmail.com>
2025-06-17 18:26:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ CHAT_ID = 1623073007
 LINK_AFILIADO = "https://s.shopee.com.br/2LMb6NCr2p?share_channel_code=1"
 # ============================
 
-# Configurar log para ver erros
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
     level=logging.INFO
@@ -22,18 +21,14 @@ async def enviar_ofertas(context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=CHAT_ID, text=texto)
 
 async def error_handler(update, context):
-    logging.error("Ocorreu um erro:", exc_info=context.error)
+    logging.error("Erro detectado", exc_info=context.error)
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
-    # Limpar webhook (caso tenha)
-    app.bot.delete_webhook()
-
-    # Agenda enviar mensagem a cada 2 minutos (120 segundos)
+    # agenda a função para rodar a cada 2 minutos
     app.job_queue.run_repeating(enviar_ofertas, interval=120, first=10)
 
-    # Adicionar manipulador de erros para registrar exceções
     app.add_error_handler(error_handler)
 
     print("Bot iniciado. Enviando ofertas a cada 2 minutos.")
@@ -41,3 +36,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
