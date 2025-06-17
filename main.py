86d3a64d22d82c1af537e0400b0eecfcdb68d0f1
EstilodoGobetti <rodrigogobettimiranda@gmail.com>
2025-06-17 10:48:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-from telegram.ext import ApplicationBuilder, CommandHandler
 from telegram import Update
-from telegram.ext import ContextTypes
-import os
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# Substitua pelo seu token do BotFather
+TOKEN = "8133241514:AAGhvnBb1uXQzR1J33_EpyL_LBxuCNmZjoE"
+
+# Comando /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Olá! Eu sou seu bot 😎")
+    await update.message.reply_text("🤖 Olá! Seu bot está funcionando com sucesso!")
 
+# Inicialização do bot
 def main():
-    app = ApplicationBuilder().token(os.getenv("TELEGRAM_TOKEN")).build()
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
