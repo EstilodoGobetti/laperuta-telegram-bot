2e36ffea1243928befa33b86db4be605c58aa2ba
EstilodoGobetti <rodrigogobettimiranda@gmail.com>
2025-06-17 13:48:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = "8133241514:AAGhvnBb1uXQzR1J33_EpyL_LBxuCNmZjoE"  # substitua pelo seu token real
+TOKEN = "8113927737:AAHKFPdD7M-9XuP44NpZrt1AcpUM0bFxolk"  # substitua pelo seu token real
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Olá, o bot está funcionando!")
